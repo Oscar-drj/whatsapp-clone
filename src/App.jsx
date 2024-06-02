c8fe9b9f5b2e66831feba97827d79c8342a430c2
OscarDelRey <oscardelreyjuarez@gmail.com>
2024-06-02 22:06:22 +0200

    Chat header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { NavBar } from './components/NavBar'
 import './App.css'
+import { MenuIcon, NewChatIcon } from './components/Icons'
 
 function App () {
   return (
     <div className='container'>
       <NavBar />
+      <div className="chats-container">
+        <div className="chats-header">
+          <h1>Chats</h1>
+          <div className="chat-actions">
+            <NewChatIcon/>
+            <MenuIcon/>
+          </div>
+        </div>
+      </div>
     </div>
 
   )
